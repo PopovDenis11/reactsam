d64142cca7f3b7963278cd9dd179de6b9b028e5f
Dan <popovdenisvl1996@gmail.com>
2020-04-26 12:54:29 +0300

    lesson 9

--- react-test-1/src/App.js
+++ react-test-1/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; //подключение МОДУЛЯ JS из папки node_modules. Без относительного пути, просто модуль
 import './App.css';
+import Header from './Header'
+import Technologies from './Technologies'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Technologies />
     </div>
-  );
+  )
+
 }
 
 export default App;
