0b0ee1621392ec1f44a1a2bf4bc04a54b208eb51
Dan <popovdenisvl1996@gmail.com>
2020-04-26 12:51:39 +0300

    lesson 9

---
+++ react-test-1/src/Technologies.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+      <div>
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>JS</li>
+          <li>React</li>
+          <li>Angular</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Technologies;
