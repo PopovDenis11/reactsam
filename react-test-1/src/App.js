e73cbd6e353b9f5ecd98b2f6699a2d4c76ec122c
Dan <popovdenisvl1996@gmail.com>
2020-04-27 20:48:34 +0300

    lesson 11

--- react-test-1/src/App.js
+++ react-test-1/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react'; //подключение МОДУЛЯ JS из папки node_modules. Без относительного пути, просто модуль
 import './App.css';
-import Header from './Header'
-import Technologies from './Technologies'
+
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="" alt='logo'/>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href="#">Profile</a>
+          </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        Main content
+      </div>
+
     </div>
   )
 
