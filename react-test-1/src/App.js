911aaabf0dd56ff0ebfdd540583af2654417e647
Dan <popovdenisvl1996@gmail.com>
2020-04-29 09:49:46 +0300

    Lesson 12

--- react-test-1/src/App.js
+++ react-test-1/src/App.js
@@ -1,33 +1,17 @@
 import React from 'react'; //подключение МОДУЛЯ JS из папки node_modules. Без относительного пути, просто модуль
 import './App.css';
-
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src="" alt='logo'/>
-      </header>
-      <nav className='nav'>
-        <div>
-          <a href="#">Profile</a>
-          </div>
-        <div>
-          <a href="#">Messages</a>
-        </div>
-        <div>
-          <a href="#">News</a>
-        </div>
-        <div>
-          <a href="#">Music</a>
-        </div>
-        <div>
-          <a href="#">Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        Main content
-      </div>
+      <Header />
+
+      <Navbar /> 
+
+      <Profile />
 
     </div>
   )
---
+++ react-test-1/src/components/Profile.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src="https://lh3.googleusercontent.com/proxy/3cyalJewn4k4FVN_zoVzKa2mE7veGIMWGqe1KZVgkINOpoc0N9NfDjIHe1ThKIHP84N98NBeDHebFkUmXeUFv2loyWekTetpFw" alt=""/>
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            view posts
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+            <div>
+              post3
+            </div>
+            <div>
+              post4
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
