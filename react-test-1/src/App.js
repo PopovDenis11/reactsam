f76b9c496c86f873dbb56a0720aab87b3cb9c23b
Dan <popovdenisvl1996@gmail.com>
2020-04-30 07:09:57 +0300

    lesson 13

--- react-test-1/src/App.js
+++ react-test-1/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react'; //подключение МОДУЛЯ JS из папки node_modules. Без относительного пути, просто модуль
 import './App.css';
-import Header from './components/Header'
-import Navbar from './components/Navbar'
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
-
       <Navbar /> 
-
       <Profile />
-
     </div>
   )
 
